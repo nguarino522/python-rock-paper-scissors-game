bb6c2daecff6e20869a68852bbe0eeb655bc01f9
Nicholas Guarino <nguarino@datto.lan>
2020-02-25 16:30:05 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+import time
+
+
+#adding in functionality later, doing 1 human player for now
+#print("Please choose 1 or 2 human players: ")
+#x = input()
+
+#print(x)1
+
+def main():
+    print("Welcome to rock, paper, scissors!")
+
+    
+    
+def 
+
+print("Enter r for rock, p for paper, and s for scissors for your choice: ")
+
+def check_input_validation(input):
+    if ((input != "r") or (input != "p") (input != "s")):
+        print("ERROR: invalid input, please try again")
+        return False
+    else:
+        return True
+
+
+print ("Thanks for playing! (program will exit in 3 seconds")
+time.sleep(3.0)
